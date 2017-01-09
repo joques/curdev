4499b0876affdd109f535533a8fdd79c2e58453b
José Quenum <josum@icloud.com>
2017-01-09 11:44:34 +0200

    added build file

--- src/util/db-manager.scala
+++ src/util/db-manager.scala
@@ -1,3 +1,12 @@
-import _
 
-def main
+trait DocumentManager {}
+
+object ArangoDBManager extends DocumentManager {}
+
+
+
+abstract class DBManager {
+    def addDoc: (containerName: String, document: AnyRef, options: AnyRef)
+    def getDoc: (containerName: String, docID: String)
+    def updateDoc: (containerName: String, docID: String, newDoc: AnyRef)
+}

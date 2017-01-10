1a14b58baaf16e23f9a6dc5c32bbfe2044e35843
José G. Quenum <josum@icloud.com>
2017-01-10 17:33:49 +0200

    Fixed the bug in the call

--- src/main/scala/Yester.scala
+++ src/main/scala/Yester.scala
@@ -2,6 +2,6 @@ object Yester {
     def main(args: Array[String]) {
         println("welcome to Yester... The resource management micro service...")
         val footer = new Foo("Tako")
-        footer.brath()
+        footer.breath()
     }
 }

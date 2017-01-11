30bb1912e1810a1a67bbaa292ef870cec733141b
José G. Quenum <josum@icloud.com>
2017-01-11 18:22:36 +0200

    Added more to the config and consumer

--- src/main/scala/Consumer.scala
+++ src/main/scala/Consumer.scala
@@ -1,12 +1,31 @@
-import kafka.consumer.{ Consumer => KafkaConsumer }
+import kafka.consumer.{ Consumer => KafkaConsumer, ConsumerIterator, WhiteList }
 import kafka.consumer._
-import kafka.serializer._
+import kafka.serializer.{DefaultDecoder, Decoder}
 import scala.collection.JavaConversions._
 import kafka.api._
 
 abstract class Consumer(topics: List[String]) {
-    protected val kafkaConfig = kafkaConfig()
+    protected val kafkaConfig = KafkaConfig()
     protected val config = new ConsumerConfig(kafkaConfig)
 
     def read(): Iterable[String]
+}
+
+case class StreamConsumer (topics: List[String]) extends Consumer(topics) {
+    private val filterSpec = new WhiteList(topics.mkString(","))
+
+    protected val keyDecoder: Decoder[Array[Byte]] = new DefaultDecoder()
+    protected val valueDecoder: Decoder[Array[Byte]] = new DefaultDecoder()
+
+    private lazy val consumer = KafkaConsumer.create(config)
+    private lazy val stream = consumer.createMessageStreamByFilter(filterSpec, 1, keyDecoder, valueDecoder).get(0)
+
+    def read(): Stream[String] = Stream.cons(new String(stream.head.message), read())
+}
+
+object StreamConsumer {
+    def apply(topics: List[String], kDecoder: Decoder[Array[Byte]], vDecoder: Decoder[Array[Byte]]) = new StreamConsumer(topics) {
+        override val keyDecoder = kDecoder
+        override val valueDecoder = vDecoder
+    }
 }
--- src/main/scala/KafkaConfig.scala
+++ src/main/scala/KafkaConfig.scala
@@ -1,4 +1,36 @@
 import java.util.Properties
-import com.typesafe.config.ConfigProperties
+import com.typesafe.config.ConfigFactory
 
-trait KafkaConfig extends Properties {}
+import KafkaConfig._
+
+trait KafkaConfig extends Properties {
+    private val consumerPrefixWithDot = consumerPrefix + "."
+    private val producerPrefixWithDot = producerPrefixWith + "."
+    private val allKeys = Seq(groupId, zookeeperConnect, brokers, serializer, partitioner, requiredAcks)
+
+    lazy val typesafeConfig = ConfigFactory.load()
+
+    allKeys.map {key =>
+        if (typesafeConfig.hasPath(key))
+            put(key.replace(consumerPrefixWithDot, "").replace(producerPrefixWithDot, ""), typesafeConfig.getString(key))
+    }
+
+    def getCustomString(key: String) = typesafeConfig.getString(key)
+    def getCustomInt(key: String) = typesafeConfig.getInt(key)
+}
+
+object KafkaConfig {
+    val consumerPrefix = "consumer"
+    val producerPrefix = "producer"
+
+    // consumer keys
+    val groupId = s"$consumerPrefix.group.id"
+    val zookeeperConnect = s"$consumerPrefix.zookeeper.connect"
+
+    val brokers = s"$producerPrefix.metadata.broker.list"
+    val serializer = s"$producerPrefix.serializer.class"
+    val partitioner = s"$producerPrefix.partitioner.class"
+    val requiredAcks = s"$producerPrefix.request.required.acks"
+
+    def apply() = new KafkaConfig {}
+}

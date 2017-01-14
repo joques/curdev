3d212698dd4311af1f1808f7ad4c5e021c64b5cc
José G. Quenum <josum@icloud.com>
2017-01-14 15:54:06 +0200

    Trying to re-arrange things

--- src/main/scala/Consumer.scala
+++ src/main/scala/Consumer.scala
@@ -6,17 +6,17 @@ import kafka.api._
 import java.util.Properties
 
 abstract class Consumer(topics: List[String]) {
-    val props = new Properties()
-    props.put("group.id", "1234")
-    props.put("zookeeper.connect", "127.0.0.1:2181")
-    props.put("auto.offset.reset", "smallest")
-    protected val config = new ConsumerConfig(props)
-
     def read(): Iterable[String]
 }
 
 case class StreamConsumer (topics: List[String]) extends Consumer(topics) {
-    private val filterSpec = new Whitelist(topics)
+    private val filterSpec = new Whitelist(topics.mkString(", "))
+
+    private val props = new Properties()
+    props.put("group.id", "1234")
+    props.put("zookeeper.connect", "127.0.0.1:2181")
+    props.put("auto.offset.reset", "smallest")
+    private val config = new ConsumerConfig(props)
 
     private lazy val consumer = KafkaConsumer.create(config)
     private lazy val stream = consumer.createMessageStreamsByFilter(filterSpec, 1, new DefaultDecoder(), new DefaultDecoder()).get(0)

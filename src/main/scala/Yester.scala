19090b2ac620a5dccfdcf0826a8d394313c598dc
José G. Quenum <josum@icloud.com>
2017-01-15 15:21:36 +0200

    checking the subscriptions

--- src/main/scala/Yester.scala
+++ src/main/scala/Yester.scala
@@ -4,7 +4,7 @@ import scala.collection.JavaConversions._
 object Yester {
     def main(args: Array[String]) {
         println("welcome to Yester... The resource management micro service...")
-        val topicList = List("find-users-req", "create-users-req")
+        val topicList = List("find-users-req","create-users-req")
         val yProducer = new Producer[String]()
         val yConsumer = new Consumer(topicList)
         println("displaying consumer and producer...")
@@ -13,7 +13,7 @@ object Yester {
         val records: ConsumerRecords[String, String] = yConsumer.read()
         println("now let's dig in...")
         println(records.count())
-        val recordIter: Iterator[ConsumerRecord[String,String]] = records.iterator()
+        val recordIter: Iterator[ConsumerRecord[String,String]] = records.records("find-users-req").iterator()
         while(recordIter.hasNext()) {
             val singleRecord = recordIter.next()
             println("showing content of current element...")

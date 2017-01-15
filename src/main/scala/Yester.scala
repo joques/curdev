3ab8226437e6c225e082e3a7a00eae0b2c946b22
José G. Quenum <josum@icloud.com>
2017-01-15 16:49:22 +0200

    tweaking more

--- src/main/scala/Yester.scala
+++ src/main/scala/Yester.scala
@@ -10,16 +10,19 @@ object Yester {
         println("displaying consumer and producer...")
         println(yConsumer.toString)
         println("calling read() from yester...")
-        val records: ConsumerRecords[String, String] = yConsumer.read()
-        println("now let's dig in...")
-        println(records.count())
-        val recordIter: Iterator[ConsumerRecord[String,String]] = records.records("find-users-req").iterator()
-        while(recordIter.hasNext()) {
-            val singleRecord = recordIter.next()
-            println("showing content of current element...")
-            println(singleRecord.key())
-            println(singleRecord.value())
-            println(singleRecord.offset())
+
+        while(true) {
+            val records: ConsumerRecords[String, String] = yConsumer.read()
+            println("now let's dig in...")
+            println(records.count())
+            val recordIter: Iterator[ConsumerRecord[String,String]] = records.records("find-users-req").iterator()
+            while(recordIter.hasNext()) {
+                val singleRecord = recordIter.next()
+                println("showing content of current element...")
+                println(singleRecord.key())
+                println(singleRecord.value())
+                println(singleRecord.offset())
+            }    
         }
     }
 }

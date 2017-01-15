357e0e67fca18be081a2a96d80825597dab1d3c9
José G. Quenum <josum@icloud.com>
2017-01-15 17:06:21 +0200

    tweaking more

--- src/main/scala/Consumer.scala
+++ src/main/scala/Consumer.scala
@@ -31,7 +31,7 @@ case class Consumer (topics: List[String]) {
 
     def read(): ConsumerRecords[String,String] = {
         println("polling the queue...")
-        val polRes: ConsumerRecords[String, String] = consumer.poll(400)
+        val polRes: ConsumerRecords[String, String] = consumer.poll(2000)
         polRes
     }
 }

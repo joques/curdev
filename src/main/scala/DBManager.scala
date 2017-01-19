8d73dd9eb8875e255230f6e12c742bf28cfcf880
José G. Quenum <josum@icloud.com>
2017-01-19 19:10:13 +0200

    working on the db manager

--- src/main/scala/DBManager.scala
+++ src/main/scala/DBManager.scala
@@ -1,6 +1,9 @@
 import scala.concurrent.ExecutionContext.Implicits.global
 import org.reactivecouchbase.ReactiveCouchbaseDriver
 
+import scala.concurrent.Future
+import play.api.libs.json._
+
 object DBManager {
   val driver = ReactiveCouchbaseDriver()
   val usersBucket = driver.bucket("yester-users")

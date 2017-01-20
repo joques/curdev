bd4001eefd700f64c7d82555f18b5de769476a98
José G. Quenum <josum@icloud.com>
2017-01-20 14:59:13 +0200

    added find user functionality to the db manager

--- src/main/scala/DBManager.scala
+++ src/main/scala/DBManager.scala
@@ -4,7 +4,13 @@ import org.reactivecouchbase.ReactiveCouchbaseDriver
 import scala.concurrent.Future
 import play.api.libs.json._
 
+case class User(username: String, password: String, profile: String, faculty: String, department: String)
+
 object DBManager {
   val driver = ReactiveCouchbaseDriver()
-  val usersBucket = driver.bucket("yester-users")
+
+  def findUser(username: String): Future[Option[User]] = {
+      val userBucket = driver.bucket("yester-users")
+      userBucket.get[User](username)
+  }
 }

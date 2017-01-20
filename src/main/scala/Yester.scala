c169baf51fd275d55f49b008f2c89b85b2767704
José G. Quenum <josum@icloud.com>
2017-01-20 15:56:53 +0200

    adjusted the main class to the new producer and consumer

--- src/main/scala/Yester.scala
+++ src/main/scala/Yester.scala
@@ -1,17 +1,17 @@
 // import org.apache.kafka.clients.consumer.{ ConsumerRecords, ConsumerRecord }
-import scala.collection.JavaConversions._
+// import scala.collection.JavaConversions._
 
 object Yester {
     def main(args: Array[String]) {
         println("welcome to Yester... The resource management micro service...")
         val topicList = List("find-users-req","create-users-req")
-        val yProducer = new Producer[String]()
+        val yProducer = new YesterProducer()
         val yConsumer = new YesterConsumer(topicList)
-        
+
         println("displaying consumer and producer...")
         println(yConsumer.toString)
         println("calling read() from yester...")
 
-        yConsumer.startConsuming()
+        yConsumer.startConsuming(yProducer)
     }
 }

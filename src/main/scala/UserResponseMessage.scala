914139c1bec34d72ef3a0ec86c4eee117cd7c910
José G. Quenum <josum@icloud.com>
2017-01-21 16:22:28 +0200

    added the missing user format

--- src/main/scala/UserResponseMessage.scala
+++ src/main/scala/UserResponseMessage.scala
@@ -3,6 +3,8 @@ import play.api.libs.json.Json
 case class UserResponseMessage(messageId: String, operationError: String, operationResult: User) extends ResponseMessage[User](messageId, operationError, operationResult)
 
 object UserResponseMessageJsonImplicits {
+    implicit val userFormat: Format[User] =  SimpleRequestMessageJsonImplicits.simpleRequestMessageFmt
+    
     implicit val userResponseMessageFmt = Json.format[UserResponseMessage]
     implicit val userResponseMessageWrites = Json.writes[UserResponseMessage]
     implicit val userResponseMessageReads = Json.reads[UserResponseMessage]

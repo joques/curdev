9d55a0b94defcdbf6de79785f653b8a45956cfb6
José G. Quenum <josum@icloud.com>
2017-01-21 17:08:42 +0200

    completed the loop

--- src/main/scala/YesterConsumer.scala
+++ src/main/scala/YesterConsumer.scala
@@ -1,6 +1,7 @@
 import java.io.Closeable
 import java.util.concurrent.{TimeUnit, Executors, ExecutorService}
 import org.apache.kafka.clients.consumer.{KafkaConsumer, ConsumerRecords, ConsumerRecord}
+import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.StringDeserializer
 import scala.collection.JavaConversions._
 import scala.util.{Failure, Success}
@@ -52,15 +53,15 @@ case class YesterConsumer (topics: List[String]) extends Closeable with Runnable
         println(message)
 
         recordTopic match {
-            case "find-users-req" => findUser(message)
-            case "create-users-req" => createUser(message)
+            case "find-users-req" => findUser(recordTopic, message)
+            case "create-users-req" => createUser(recordTopic, message)
             case _ => println("unknown topic...")
         }
 
         println("delving into the message -- end")
     }
 
-    def findUser(message: SimpleRequestMessage): Unit = {
+    def findUser(topic: String, message: SimpleRequestMessage): Unit = {
         val userName = message.content
         println(s"finding user $userName")
         val userResult = DBManager.findUser("kunta")
@@ -70,17 +71,19 @@ case class YesterConsumer (topics: List[String]) extends Closeable with Runnable
                 val userSuccessRespMsg: UserResponseMessage = new UserResponseMessage(message.messageId, None, userVal)
                 val succMsgStr = Json.toJson(userSuccessRespMsg).toString()
                 println(s"the success message to be sent is $succMsgStr")
+                messenger.getProducer().send(new ProducerRecord[String,String](topic, succMsgStr))
             }
             case Failure(userErr) => {
                 userErr.printStackTrace
                 val userErrorRespMsg: UserResponseMessage = new UserResponseMessage(message.messageId, Option(userErr.getMessage), None)
                 val errMsgStr = Json.toJson(userErrorRespMsg).toString()
                 println(s"the error message to be sent it $errMsgStr")
+                messenger.getProducer().send(new ProducerRecord[String,String](topic, errMsgStr))
             }
         }
     }
 
-    def createUser(message: SimpleRequestMessage): Unit = {
+    def createUser(topic: String, message: SimpleRequestMessage): Unit = {
         println("creating new user")
     }
 

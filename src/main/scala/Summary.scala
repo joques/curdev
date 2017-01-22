e40168d35e38673b467ae19e757a7e2b4d3ee9d6
José G. Quenum <josum@icloud.com>
2017-01-22 18:29:45 +0200

    added the summary

---
+++ src/main/scala/Summary.scala
@@ -0,0 +1,11 @@
+import play.api.libs.json.Json
+
+case class Summary(inProgress: Option[List[Programme]], dueForReview: Option[List[Programme]], recentlyApproved: Option[List[Programme]])
+
+object SummaryJsonImplicits {
+    implicit val prgFormat: Format[Programme] =  ProgrammeJsonImplicits.prgFmt
+
+    implicit val summaryFmt = Json.format[Summary]
+    implicit val summaryWrites = Json.writes[Summary]
+    implicit val summaryReads = Json.reads[Summary]
+}

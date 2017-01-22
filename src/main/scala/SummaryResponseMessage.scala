53966c2bed36f972c4c54c7c845e4169fa0523d0
José G. Quenum <josum@icloud.com>
2017-01-22 18:36:58 +0200

    added summary response message

---
+++ src/main/scala/SummaryResponseMessage.scala
@@ -0,0 +1,11 @@
+import play.api.libs.json.{Reads, Json, Format}
+
+case class SummaryResponseMessage(messageId: String, operationError: Option[String], operationResult: Option[Summary]) extends ResponseMessage[Summary](messageId, operationError, operationResult)
+
+object SummaryResponseMessageJsonImplicits {
+    implicit val summaryFormat: Format[Summary] =  SummaryJsonImplicits.summaryFmt
+
+    implicit val summaryResponseMessageFmt = Json.format[SummaryResponseMessage]
+    implicit val summaryResponseMessageWrites = Json.writes[SummaryResponseMessage]
+    implicit val summaryResponseMessageReads = Json.reads[SummaryResponseMessage]
+}

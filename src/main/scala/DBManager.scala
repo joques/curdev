000a1809c3fa03f01ec192f0e31b166bfeec5e56
José G. Quenum <josum@icloud.com>
2017-01-23 13:53:47 +0200

    reverted back to non generic form of method

--- src/main/scala/DBManager.scala
+++ src/main/scala/DBManager.scala
@@ -7,20 +7,8 @@ object DBManager {
   val driver = ReactiveCouchbaseDriver()
   implicit val userFormat: Format[User] = UserJsonImplicits.userFmt
 
-  def findId[T](key: String, bucketName: String): Future[Option[T]] = {
-      val bucket = driver.bucket(bucketName)
-      bucket.get[T](key)
-  }
-
-  def findUser(username: String): Future[Option[User]] = findId[User](username, "yester-users")
-
-  // def findUser(username: String): Future[Option[User]] = {
-  //     val userBucket = driver.bucket("yester-users")
-  //     userBucket.get[User](username)
-  // }
-
-  def findAllProgrammes(): Future[List[Programme]] = {
-      val programmeBucket = driver.bucket("yester-programmes")
-      programmeBucket.find[Programme]
+  def findUser(username: String): Future[Option[User]] = {
+      val userBucket = driver.bucket("yester-users")
+      userBucket.get[User](username)
   }
 }

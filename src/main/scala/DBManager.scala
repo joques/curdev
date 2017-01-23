cc977cad14ce8b0655092b4d8475f46980d5ad6f
José G. Quenum <josum@icloud.com>
2017-01-23 12:33:51 +0200

    parameterized the findId method

--- src/main/scala/DBManager.scala
+++ src/main/scala/DBManager.scala
@@ -7,10 +7,17 @@ object DBManager {
   val driver = ReactiveCouchbaseDriver()
   implicit val userFormat: Format[User] = UserJsonImplicits.userFmt
 
-  def findUser(username: String): Future[Option[User]] = {
-      val userBucket = driver.bucket("yester-users")
-      userBucket.get[User](username)
+  def findId(key: String, bucketName: String): Future[Option[T]] = {
+      val bucket = driver.bucket(bucketName)
+      bucket.get[T](key)
   }
+
+  def findUser(username: String): Future[Option[User]] = findId[User](username, "yester-users")
+
+  // def findUser(username: String): Future[Option[User]] = {
+  //     val userBucket = driver.bucket("yester-users")
+  //     userBucket.get[User](username)
+  // }
 
   def findAllProgrammes(): Future[List[Programme]] = {
       val programmeBucket = driver.bucket("yester-programmes")

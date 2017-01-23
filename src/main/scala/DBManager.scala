95833937e6799931b8c51d454f8c0163ddcf2dd4
José G. Quenum <josum@icloud.com>
2017-01-23 16:08:55 +0200

    made the find method a parameterized

--- src/main/scala/DBManager.scala
+++ src/main/scala/DBManager.scala
@@ -7,8 +7,15 @@ object DBManager {
   val driver = ReactiveCouchbaseDriver()
   implicit val userFormat: Format[User] = UserJsonImplicits.userFmt
 
-  def findUser(username: String): Future[Option[User]] = {
-      val userBucket = driver.bucket("yester-users")
-      userBucket.get[User](username)
+  // def findUser(username: String): Future[Option[User]] = {
+  //     val userBucket = driver.bucket("yester-users")
+  //     userBucket.get[User](username)
+  // }
+
+  def findUser(username: String): Future[Option[User]] = findById[User](username, "yester-users")
+
+  def findById[T](docKey: String, bucketName: String, implicit valFormat: Format[T]): Future[Option[T]] = {
+      val curBucket = driver.bucket(bucketName)
+      curBucket.get[T](docKey)
   }
 }

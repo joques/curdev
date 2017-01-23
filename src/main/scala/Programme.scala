8fc947f44057de58d3367aaa4d4ec10e6c853040
José G. Quenum <josum@icloud.com>
2017-01-23 18:06:14 +0200

    removed special character from attribute names

--- src/main/scala/Programme.scala
+++ src/main/scala/Programme.scala
@@ -1,6 +1,6 @@
 import play.api.libs.json.Json
 
-case class Programme(faculty: String, department: String, title: String, status: String, approved-on: String, next-review: String, history: List[String])
+case class Programme(faculty: String, department: String, title: String, status: String, approvedOn: String, nextReview: String, history: List[String])
 
 object ProgrammeJsonImplicits {
     implicit val prgFmt = Json.format[Programme]

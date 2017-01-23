a2de3217cb3165fd60eba411b2efaac80ea04198
José G. Quenum <josum@icloud.com>
2017-01-23 16:40:13 +0200

    added the parameterized version of find all

--- src/main/scala/DBManager.scala
+++ src/main/scala/DBManager.scala
@@ -7,15 +7,17 @@ object DBManager {
   val driver = ReactiveCouchbaseDriver()
   implicit val userFormat: Format[User] = UserJsonImplicits.userFmt
 
-  // def findUser(username: String): Future[Option[User]] = {
-  //     val userBucket = driver.bucket("yester-users")
-  //     userBucket.get[User](username)
-  // }
-
   def findUser(username: String): Future[Option[User]] = findById[User](username, "yester-users")
+
+  def findAllProgrammes(designDoc: String, viewName: String): Future[Option[List[Programme]]] = findAll[Programme]("yester-programmes", "progr_dd", "progr")
 
   def findById[T](docKey: String, bucketName: String)(implicit valFormat: Format[T]): Future[Option[T]] = {
       val curBucket = driver.bucket(bucketName)
       curBucket.get[T](docKey)
+  }
+
+  def findAll[T](bucketName: String, designDoc: String, viewName: String)(implicit valFormat: Format[T]): Future[Option[List[T]]] = {
+      val curBucket = driver.bucket(bucketName)
+      curBucket.find[T](designDoc, viewName)(new Query().setIncludeDocs(true).setStale(Stale.FALSE))
   }
 }

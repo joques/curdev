12ac96468095cb7688ecf07c783bfc65421388ae
José G. Quenum <josum@icloud.com>
2017-02-22 09:52:44 +0200

    Added a new type combining users with pre programme codes

---
+++ src/main/scala/UserWithPreProgramme.scala
@@ -0,0 +1,11 @@
+import play.api.libs.json.{Json, Format}
+
+case class UserWithPreProgramme(userDetails: User, preProgrammeCodes: Option[List[String]])
+
+object UserWithPreProgrammeJsonImplicits {
+    implicit val userFormat: Format[User] =  UserJsonImplicits.userFmt
+
+    implicit val uwPPFmt = Json.format[UserWithPreProgramme]
+    implicit val uwPPWrites = Json.writes[UserWithPreProgramme]
+    implicit val uwPPReads = Json.reads[UserWithPreProgramme]
+}
---
+++ src/main/scala/UserWithPreProgrammeResponseMessage.scala
@@ -0,0 +1,11 @@
+import play.api.libs.json.{Reads, Json, Format}
+
+case class UserWithPreProgrammeResponseMessage(messageId: String, operationError: Option[String], operationResult: Option[UserWithPreProgramme]) extends ResponseMessage[UserWithPreProgramme](messageId, operationError, operationResult)
+
+object UserResponseMessageJsonImplicits {
+    implicit val uwPPFmt: Format[UserWithPreProgramme] =  UserWithPreProgrammeJsonImplicits.uwPPFmt
+
+    implicit val uwPPResponseMessageFmt = Json.format[UserWithPreProgrammeResponseMessage]
+    implicit val uwPPResponseMessageWrites = Json.writes[UserWithPreProgrammeResponseMessage]
+    implicit val uwPPResponseMessageReads = Json.reads[UserWithPreProgrammeResponseMessage]
+}

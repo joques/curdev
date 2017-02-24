b58348f8ea87ab2a7717cb700574df6cfbec1b8c
José G. Quenum <josum@icloud.com>
2017-02-24 18:41:59 +0200

    checking the loader

--- src/main/scala/Yester.scala
+++ src/main/scala/Yester.scala
@@ -5,8 +5,9 @@ object Yester {
     def main(args: Array[String]) {
         println("welcome to Yester... The resource management micro service...")
         val topicList = List("find-users-req","create-users-req", "summary-req", "need-analysis-start-req")
-        val yProducer = new YesterProducer()
+
         val yConsumer = new YesterConsumer(topicList)
+        val yProducer = new YesterProducer()
 
         println("displaying consumer and producer...")
         println(yConsumer.toString)

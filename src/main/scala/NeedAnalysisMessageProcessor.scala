79390077d65efb6e13ffa749ed52fd0769bac2c1
José G. Quenum <josum@icloud.com>
2017-02-25 18:44:12 +0200

    shifted from case class to class

--- src/main/scala/NeedAnalysisMessageProcessor.scala
+++ src/main/scala/NeedAnalysisMessageProcessor.scala
@@ -1,5 +1,6 @@
 import akka.actor._
-case class NeedAnalysisMessageProcessor extends MessageProcessor {
+
+class NeedAnalysisMessageProcessor extends MessageProcessor {
     def receive = {
         case YesterProducer(yProd) =>
             messenger = yProd

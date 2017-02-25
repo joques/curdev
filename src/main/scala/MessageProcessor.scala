a1777fedaf8970ac9a8d2e5dd1f66bed09407505
José G. Quenum <josum@icloud.com>
2017-02-25 19:02:56 +0200

    building up the need analysis message processor

--- src/main/scala/MessageProcessor.scala
+++ src/main/scala/MessageProcessor.scala
@@ -1,3 +1,30 @@
 import akka.actor._
+import org.reactivecouchbase.client.OpResult
+import play.api.libs.json.{Reads, Json, Writes}
 
-abstract class MessageProcessor(messenger: YesterProducer) extends Actor {}
+abstract class MessageProcessor(messenger: YesterProducer) extends Actor {
+    def handleInsertionResultWithSimpleResponse(result: Future[OpResult], messageId: String, responseTopic: String): Unit = {
+        result.onComplete {
+            case Success(succOpRes) => {
+                if (succOpRes.isSuccess) {
+                    val simpleSuccessRespMsg: SimpleResponseMessage = new SimpleResponseMessage(messageId, None, Option(succOpRes.getMessage))
+                    val succMsgStr = Json.toJson(simpleSuccessRespMsg).toString()
+                    println(s"the success message to be sent is $succMsgStr")
+                    messenger.getProducer().send(new ProducerRecord[String,String](responseTopic, succMsgStr))
+                }
+                else {
+                    val simpleErrorRespMsg1: SimpleResponseMessage = new SimpleResponseMessage(messageId, Option(succOpRes.getMessage), None)
+                    val errMsgStr1 = Json.toJson(simpleErrorRespMsg1).toString()
+                    println(s"the error message to be sent out is $errMsgStr1")
+                    messenger.getProducer().send(new ProducerRecord[String,String](responseTopic, errMsgStr1))
+                }
+            }
+            case Failure(failOpRes) => {
+                val simpleErrorRespMsg: SimpleResponseMessage = new SimpleResponseMessage(messageId, Option(failOpRes.getMessage), None)
+                val errMsgStr = Json.toJson(simpleErrorRespMsg).toString()
+                println(s"the error message to be sent out is $errMsgStr")
+                messenger.getProducer().send(new ProducerRecord[String,String](responseTopic, errMsgStr))
+            }
+        }
+    }
+}
--- src/main/scala/NeedAnalysisMessageProcessor.scala
+++ src/main/scala/NeedAnalysisMessageProcessor.scala
@@ -1,8 +1,23 @@
 import akka.actor._
+import play.api.libs.json.{Reads, Json, Writes}
 
 case class NeedAnalysisMessageProcessor(messenger: YesterProducer) extends MessageProcessor(messenger) {
     def receive = {
         case "need-analysis-start-req" =>
             println("received need-analysis-start-req message")
+            val needAnalysisStartMessage = Json.parse(recordValue).as[ProgrammeRequestMessage]
+            createPreProgramme(needAnalysisStartMessage)
+        case "need-analysis-consult-req" =>
+            println("received need-analysis-consult-req message")
+    }
+
+    def createPreProgramme(message: ProgrammeRequestMessage): Unit = {
+        println("creating a new programme object ...")
+
+        val progObj = message.content
+        val progKey = UUID.randomUUID().toString()
+        val createProgOpRes = DBManager.createProgramme(progKey, progObj)
+
+        handleInsertionResultWithSimpleResponse(createProgOpRes, message.messageId, "need-analysis-start-res")
     }
 }

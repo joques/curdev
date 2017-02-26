8818442d5ac6e01a6f44c10f91bfaf842982f37f
José G. Quenum <josum@icloud.com>
2017-02-26 15:36:19 +0200

    fixing bugs

--- src/main/scala/CreateUserRequestMessage.scala
+++ src/main/scala/CreateUserRequestMessage.scala
@@ -1,11 +1,11 @@
-import play.api.libs.json.Json
+import play.api.libs.json.{Json, Format}
 
 case class CreateUserRequestMessage (simpleMsg: SimpleRequestMessage){}
 
 object CreateUserRequestMessageJsonImplicits {
     implicit val simpleReqMsgFormat: Format[SimpleRequestMessage] =  SimpleRequestMessageJsonImplicits.simpleRequestMessageFmt
 
-    implicit val cuRequestMessageFmt = Json.format[FindUserRequestMessage]
-    implicit val cuRequestMessageWrites = Json.writes[FindUserRequestMessage]
-    implicit val cuRequestMessageReads = Json.reads[FindUserRequestMessage]
+    implicit val cuRequestMessageFmt = Json.format[CreateUserRequestMessage]
+    implicit val cuRequestMessageWrites = Json.writes[CreateUserRequestMessage]
+    implicit val cuRequestMessageReads = Json.reads[CreateUserRequestMessage]
 }

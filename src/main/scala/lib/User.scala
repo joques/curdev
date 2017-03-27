0ce9cbe8ff8d297ecbc2228641112d2bd21b0f36
José G. Quenum <josum@icloud.com>
2017-03-27 15:22:13 +0200

    added more fields to user representation

--- src/main/scala/lib/User.scala
+++ src/main/scala/lib/User.scala
@@ -2,7 +2,7 @@ package yester.lib
 
 import play.api.libs.json.Json
 
-final case class User(username: String, password: String, profile: String, faculty: Int, department: Int)
+final case class User(username: String, password: String, profile: String, firstname: String, lastname: String, faculty: Int, department: Int)
 
 object UserJsonImplicits {
     implicit val userFmt = Json.format[User]

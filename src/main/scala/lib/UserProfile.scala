f5341dda4949322a2e6bfbe757d08e03473388ac
José G. Quenum <josum@icloud.com>
2017-04-03 09:28:11 +0100

    added more profiles

--- src/main/scala/lib/UserProfile.scala
+++ src/main/scala/lib/UserProfile.scala
@@ -2,5 +2,5 @@ package yester.lib
 
 Object UserProfile extends Enumeration {
     type UserProfile = Value
-    val DEAN, ADEAN, VC, DVCA, REG, HOD, DHOD, CDC, PAC, TLU, QAU, PDU, BOS, SENATE = Value
+    val DEAN, ADEAN, VC, DVCA, REG, HOD, DHOD, CDC, PAC, TLU, QAU, PDU, BOS, SENATE, CDE, HPGL, NGCL, CEU = Value
 }

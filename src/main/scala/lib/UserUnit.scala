d60262a4ce47244d2e9affa6ba67c051ecd17a8b
José G. Quenum <josum@icloud.com>
2017-04-03 18:28:39 +0100

    still battling with the format

--- src/main/scala/lib/UserUnit.scala
+++ src/main/scala/lib/UserUnit.scala
@@ -1,13 +1,13 @@
 package yester.lib
 
 // import play.api.libs.json.{Format}
-import julienrf.json.derived.flat
+import julienrf.json.derived
 
 sealed trait UserUnit
 
 case class AcademicUnit(faculty: Int, department: Int) extends UserUnit
 case class AdministrativeUnit(office: Int, section: Int) extends UserUnit
 
-object UserUnitJsonImplicits {
-    implicit lazy val userUnitFmt: OFormat[UserUnit] = flat.oformat((__ \ "type").format[String])
+object UserUnit {
+    implicit val userUnitFmt: OFormat[UserUnit] = derived.oformat[UserUnit]
 }

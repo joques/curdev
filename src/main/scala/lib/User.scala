1c9e6b1e045470eb36a08a16099272c6ef5c2d16
José G. Quenum <josum@icloud.com>
2017-04-03 09:36:47 +0100

    changed user to accomodate the changes in user profile and user unit

--- src/main/scala/lib/User.scala
+++ src/main/scala/lib/User.scala
@@ -2,7 +2,7 @@ package yester.lib
 
 import play.api.libs.json.Json
 
-final case class User(username: String, password: String, profile: String, firstname: String, lastname: String, faculty: Int, department: Int)
+final case class User(username: String, password: String, profile: UserProfile, firstname: String, lastname: String, unit: UserUnit)
 
 object UserJsonImplicits {
     implicit val userFmt = Json.format[User]

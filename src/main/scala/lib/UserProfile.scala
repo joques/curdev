64a18939cfc13cae132953fc96d76eeffefb4381
José G. Quenum <josum@icloud.com>
2017-04-03 10:41:38 +0100

    modified the formating of user profile

--- src/main/scala/lib/UserProfile.scala
+++ src/main/scala/lib/UserProfile.scala
@@ -1,10 +1,32 @@
 package yester.lib
 
-import play.api.libs.json.Json
+import play.api.libs.json._
+
+object EnumUtils {
+    def enumReads[E <: Enumeration](enum: E): Reads[E#Value] = new Reads[E#Value] {
+        def reads(json: JsValue): JsResult[E#Value] = json match {
+            case JsString(s) => {
+                try {
+                    JsSuccess(enum.withName(s))
+                } catch {
+                    case _: NoSuchElementException => JsError(s"Enumeration expected of type: '${enum.getClass}', but it does not appear to contain the value: '$s'")
+                }
+            }
+            case _ => JsError("String value expected")
+        }
+    }
+    implicit def enumWrites[E <: Enumeration]: Writes[E#Value] = new Writes[E#Value] {
+        def writes(v: E#Value): JsValue = JsString(v.toString)
+    }
+
+    implicit def enumFormat[E <: Enumeration](enum: E): Format[E#Value] = {
+        Format(EnumUtils.enumReads(enum), EnumWriter.enumWrites)
+    }
+}
 
 object UserProfile extends Enumeration {
     type UserProfile = Value
     val DEAN, ADEAN, VC, DVCA, REG, HOD, DHOD, CDC, PAC, TLU, QAU, PDU, BOS, SENATE, CDE, HPGL, NGCL, CEU = Value
 
-    implicit val userProfileEnum = Json.format[UserProfile]
+    implicit val userProfileFormat = EnumUtils.enumFormat(UserProfile)
 }

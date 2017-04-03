b6552fa7605df1a1110444cd908894fdd8ac3e21
José G. Quenum <josum@icloud.com>
2017-04-03 10:50:27 +0100

    cahnged the format and json generation in user unit

--- src/main/scala/lib/User.scala
+++ src/main/scala/lib/User.scala
@@ -6,10 +6,9 @@ import UserProfile._
 final case class User(username: String, password: String, profile: UserProfile, firstname: String, lastname: String, unit: UserUnit)
 
 object UserJsonImplicits {
-    implicit val academicUnitFormat: Format[AcademicUnit] =  AcademicUnitJsonImplicits.acaUnitFmt
-    implicit val administrativeUnitFormat: Format[AdministrativeUnit] =  AdministrativeUnitJsonImplicits.adUnitFmt
+    // implicit val academicUnitFormat: Format[AcademicUnit] =  AcademicUnitJsonImplicits.acaUnitFmt
+    // implicit val administrativeUnitFormat: Format[AdministrativeUnit] =  AdministrativeUnitJsonImplicits.adUnitFmt
     implicit val userUnitFormat: Format[UserUnit] =  UserUnitJsonImplicits.userUnitFmt
-    // implicit val userProfileFormat =  UserProfile.userProfileFormat
 
     implicit val userFmt = Json.format[User]
     implicit val userWrites = Json.writes[User]
--- src/main/scala/lib/UserUnit.scala
+++ src/main/scala/lib/UserUnit.scala
@@ -1,6 +1,6 @@
 package yester.lib
 
-import play.api.libs.json.Json
+import play.api.libs.json.{Format, Variants}
 
 sealed trait UserUnit
 
@@ -8,18 +8,18 @@ case class AcademicUnit(faculty: Int, department: Int) extends UserUnit
 case class AdministrativeUnit(office: Int, section: Int) extends UserUnit
 
 object UserUnitJsonImplicits {
-    implicit val userUnitFmt = Json.format[UserUnit]
+    implicit val userUnitFmt: Format[UserUnit] = Variants.format[UserUnit]
 }
 
-object AcademicUnitJsonImplicits {
-    implicit val acaUnitFmt = Json.format[AcademicUnit]
-    implicit val acaUnitWrites = Json.writes[AcademicUnit]
-    implicit val acaUnitReads = Json.reads[AcademicUnit]
-}
-
-
-object AdministrativeUnitJsonImplicits {
-    implicit val adUnitFmt = Json.format[AdministrativeUnit]
-    implicit val adUnitWrites = Json.writes[AdministrativeUnit]
-    implicit val adUnitReads = Json.reads[AdministrativeUnit]
-}
+// object AcademicUnitJsonImplicits {
+//     implicit val acaUnitFmt = Json.format[AcademicUnit]
+//     implicit val acaUnitWrites = Json.writes[AcademicUnit]
+//     implicit val acaUnitReads = Json.reads[AcademicUnit]
+// }
+//
+//
+// object AdministrativeUnitJsonImplicits {
+//     implicit val adUnitFmt = Json.format[AdministrativeUnit]
+//     implicit val adUnitWrites = Json.writes[AdministrativeUnit]
+//     implicit val adUnitReads = Json.reads[AdministrativeUnit]
+// }

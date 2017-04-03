d4974f8103b5572f140492984a03833aa9aeaab3
José G. Quenum <josum@icloud.com>
2017-04-03 09:52:45 +0100

    added missing refs

--- src/main/scala/lib/User.scala
+++ src/main/scala/lib/User.scala
@@ -1,10 +1,14 @@
 package yester.lib
 
 import play.api.libs.json.Json
+import UserProfile._
 
 final case class User(username: String, password: String, profile: UserProfile, firstname: String, lastname: String, unit: UserUnit)
 
 object UserJsonImplicits {
+    implicit val academicUnitFormat: Format[AcademicUnit] =  AcademicUnitJsonImplicits.acaUnitFmt
+    implicit val administrativeUnitFormat: Format[AdministrativeUnit] =  AdministrativeUnitJsonImplicits.adUnitFmt
+
     implicit val userFmt = Json.format[User]
     implicit val userWrites = Json.writes[User]
     implicit val userReads = Json.reads[User]

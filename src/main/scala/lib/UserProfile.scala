891d6d56b603151fc8d05b41e6dbcccfab2ae00f
José G. Quenum <josum@icloud.com>
2017-04-03 09:11:10 +0100

    completed the list of profile names

--- src/main/scala/lib/UserProfile.scala
+++ src/main/scala/lib/UserProfile.scala
@@ -2,5 +2,5 @@ package yester.lib
 
 Object UserProfile extends Enumeration {
     type UserProfile = Value
-    val dean, adean, pdu, hod = Value
+    val DEAN, ADEAN, VC, DVCA, REG, HOD, DHOD, CDC, PAC, TLU, QAU, PDU, BOS, SENATE = Value
 }

82c84cfba6ad927234cbd15bf44143278d561c87
José G. Quenum <josum@icloud.com>
2017-04-03 18:19:02 +0100

    addded the flat part of derived

--- src/main/scala/lib/User.scala
+++ src/main/scala/lib/User.scala
@@ -6,8 +6,6 @@ import UserProfile._
 final case class User(username: String, password: String, profile: UserProfile, firstname: String, lastname: String, unit: UserUnit)
 
 object UserJsonImplicits {
-    // implicit val academicUnitFormat: Format[AcademicUnit] =  AcademicUnitJsonImplicits.acaUnitFmt
-    // implicit val administrativeUnitFormat: Format[AdministrativeUnit] =  AdministrativeUnitJsonImplicits.adUnitFmt
     implicit val userUnitFormat: Format[UserUnit] =  UserUnitJsonImplicits.userUnitFmt
 
     implicit val userFmt = Json.format[User]
--- src/main/scala/lib/UserUnit.scala
+++ src/main/scala/lib/UserUnit.scala
@@ -1,7 +1,7 @@
 package yester.lib
 
 // import play.api.libs.json.{Format}
-import julienrf.json.derived._
+import julienrf.json.derived.flat
 
 sealed trait UserUnit
 
@@ -11,16 +11,3 @@ case class AdministrativeUnit(office: Int, section: Int) extends UserUnit
 object UserUnitJsonImplicits {
     implicit lazy val userUnitFmt: OFormat[UserUnit] = flat.oformat((__ \ "type").format[String])
 }
-
-// object AcademicUnitJsonImplicits {
-//     implicit val acaUnitFmt = Json.format[AcademicUnit]
-//     implicit val acaUnitWrites = Json.writes[AcademicUnit]
-//     implicit val acaUnitReads = Json.reads[AcademicUnit]
-// }
-//
-//
-// object AdministrativeUnitJsonImplicits {
-//     implicit val adUnitFmt = Json.format[AdministrativeUnit]
-//     implicit val adUnitWrites = Json.writes[AdministrativeUnit]
-//     implicit val adUnitReads = Json.reads[AdministrativeUnit]
-// }

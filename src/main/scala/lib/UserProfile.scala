284e20b9d301f1f30a3546d9f9cb9a2c0beddd7c
José G. Quenum <josum@icloud.com>
2017-04-03 09:34:39 +0100

    introduced a separation between academic unit and administrative unit

--- src/main/scala/lib/UserProfile.scala
+++ src/main/scala/lib/UserProfile.scala
@@ -1,6 +1,6 @@
 package yester.lib
 
-Object UserProfile extends Enumeration {
+object UserProfile extends Enumeration {
     type UserProfile = Value
     val DEAN, ADEAN, VC, DVCA, REG, HOD, DHOD, CDC, PAC, TLU, QAU, PDU, BOS, SENATE, CDE, HPGL, NGCL, CEU = Value
 }
---
+++ src/main/scala/lib/UserUnit.scala
@@ -0,0 +1,21 @@
+package yester.lib
+
+import play.api.libs.json.Json
+
+sealed trait UserUnit
+
+case class AcademicUnit(faculty: Int, department: Int) extends UserUnit
+case class AdministrativeUnit(office: Int, section: Int) extends UserUnit
+
+object AcademicUnitJsonImplicits {
+    implicit val acaUnitFmt = Json.format[AcademicUnit]
+    implicit val acaUnitWrites = Json.writes[AcademicUnit]
+    implicit val acaUnitReads = Json.reads[AcademicUnit]
+}
+
+
+object AdministrativeUnitJsonImplicits {
+    implicit val adUnitFmt = Json.format[AdministrativeUnit]
+    implicit val adUnitWrites = Json.writes[AdministrativeUnit]
+    implicit val adUnitReads = Json.reads[AdministrativeUnit]
+}

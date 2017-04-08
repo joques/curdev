c69a9435a1fe802753ba82f1f5131408955bcec9
José G. Quenum <josum@icloud.com>
2017-04-08 20:03:54 +0100

    transitioning to reactive souchbase ver 2

--- src/main/scala/util/DBManager.scala
+++ src/main/scala/util/DBManager.scala
@@ -52,52 +52,19 @@ object DBManager {
         val curBucket = driver.bucket(bucketName)
         curBucket.get[T](docKey)
     }
-}
 
+    def findAll[T](bucketName: String): Future[List[T]] = {
+        val curBucket = driver.bucket(bucketName)
+        val query = s"select * from $bucketName"
+        curBucket.search(N1qlQuery(query)).asSeq
+    }
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import org.reactivecouchbase.ReactiveCouchbaseDriver
-import scala.concurrent.Future
-import org.reactivecouchbase.client.{OpResult, Constants}
-import com.couchbase.client.protocol.views.{Stale, Query}
-import play.api.libs.json.{Json, Format, Writes}
-import net.spy.memcached.{PersistTo, ReplicateTo}
-import yester.lib.{User, UserJsonImplicits, Programme, ProgrammeJsonImplicits, NeedAnalysisConsultation, NeedAnalysisConsultationJsonImplicits, NeedAnalysisSurvey, NeedAnalysisSurveyJsonImplicits}
+    def findUser(username: String): Future[Option[User]] = findById[User]("yester-users", username)
 
-object DBManager {
-  val driver = ReactiveCouchbaseDriver()
-  implicit val userFormat: Format[User] = UserJsonImplicits.userFmt
-  implicit val progFormat: Format[Programme] = ProgrammeJsonImplicits.prgFmt
-  implicit val progWriter: Writes[Programme] = ProgrammeJsonImplicits.prgWrites
-  implicit val naConsFormat: Format[NeedAnalysisConsultation] = NeedAnalysisConsultationJsonImplicits.needAnaConsFmt
-  implicit val naConsWriter: Writes[NeedAnalysisConsultation] = NeedAnalysisConsultationJsonImplicits.needAnaConsWrites
-  implicit val naSurvFormat: Format[NeedAnalysisSurvey] = NeedAnalysisSurveyJsonImplicits.needAnaSurvFmt
-  implicit val naSurvWriter: Writes[NeedAnalysisSurvey] = NeedAnalysisSurveyJsonImplicits.needAnaSurvWrites
+    def findAllProgrammes(): Future[List[Programme]] = findAll[Programme]("yester-programmes")
 
-  def findUser(username: String): Future[Option[User]] = findById[User]("yester-users", username)
+    def createProgramme(progKey: String, progData: Programme): Future[Programme] = save[Programme]("yester-programmes", progKey, progData)
 
-  def findAllProgrammes(): Future[List[Programme]] = findAll[Programme]("yester-programmes", "progr_dd", "prog")
-
-  def createProgramme(progKey: String, progData: Programme): Future[OpResult] = save[Programme]("yester-programmes", progKey, progData)
-
-  def addNeedAnalysisConsultation(consulationKey: String, consultationData: NeedAnalysisConsultation): Future[OpResult] = save[NeedAnalysisConsultation]("yester-consultations", consulationKey, consultationData)
-
-  def addNeedAnalysisSurvey(surveyKey: String, surveyObj: NeedAnalysisSurvey): Future[OpResult] = save[NeedAnalysisSurvey]("yester-na-surveys", surveyKey, surveyObj)
-
-
-
-  def findById[T](bucketName: String, docKey: String)(implicit valFormat: Format[T]): Future[Option[T]] = {
-      val curBucket = driver.bucket(bucketName)
-      curBucket.get[T](docKey)
-  }
-
-  def findAll[T](bucketName: String, designDoc: String, viewName: String)(implicit valFormat: Format[T]): Future[List[T]] = {
-      val curBucket = driver.bucket(bucketName)
-      curBucket.find[T](designDoc, viewName)(new Query().setIncludeDocs(true).setStale(Stale.FALSE))
-  }
-
-  def save[T](bucketName: String, key: String, data: T)(implicit valFormat: Format[T]): Future[OpResult] = {
-      val curBucket = driver.bucket(bucketName)
-      curBucket.set(key, data, Constants.expiration, PersistTo.ZERO, ReplicateTo.ZERO)
-  }
+    def addNeedAnalysisConsultation(consulationKey: String, consultationData: NeedAnalysisConsultation): Future[NeedAnalysisConsultation] = save[NeedAnalysisConsultation]("yester-consultations", consulationKey, consultationData)
+    def addNeedAnalysisSurvey(surveyKey: String, surveyObj: NeedAnalysisSurvey): Future[NeedAnalysisSurvey] = save[NeedAnalysisSurvey]("yester-na-surveys", surveyKey, surveyObj)
 }

a84d79b76ac55d5692ee0a54287aa250a788db76
José G. Quenum <josum@icloud.com>
2017-09-19 09:30:39 +0200

    added building blocks for PAC committee members

---
+++ src/main/scala/lib/PACCommitteeMembers.scala
@@ -0,0 +1,13 @@
+package yester.lib
+
+import play.api.libs.json.Json
+
+final case class PACCommitteeMembers(devCode: String, members: List[SinglePACCommitteeMember])
+
+object PACCommitteeMembersJsonImplicits {
+    implicit val singlePACCommitteeMemberFormat: Format[SinglePACCommitteeMember] = SinglePACCommitteeMemberJsonImplicits.singlePACComFmt
+
+    implicit val pacCmtMembersFmt = Json.format[PACCommitteeMembers]
+    implicit val pacCmtMembersWrites = Json.writes[PACCommitteeMembers]
+    implicit val pacCmtMembersReads = Json.reads[PACCommitteeMembers]
+}

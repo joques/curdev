9332cc7bb8002cf755f97f7617ea19ae119300dd
José G. Quenum <josum@icloud.com>
2017-09-19 20:02:38 +0200

    added db access to curricula-dev bucket

--- src/main/scala/util/DBManager.scala
+++ src/main/scala/util/DBManager.scala
@@ -7,7 +7,7 @@ import org.reactivecouchbase.client.{OpResult, Constants}
 import com.couchbase.client.protocol.views.{Stale, Query}
 import play.api.libs.json.{Json, Format, Writes}
 import net.spy.memcached.{PersistTo, ReplicateTo}
-import yester.lib.{User, UserJsonImplicits, Programme, ProgrammeJsonImplicits, NeedAnalysis, NeedAnalysisJsonImplicits}
+import yester.lib.{User, UserJsonImplicits, Programme, ProgrammeJsonImplicits, NeedAnalysis, NeedAnalysisJsonImplicits, CurriculumDevelopment, CurriculumDevelopmentJsonImplicits}
 
 object DBManager {
   val driver = ReactiveCouchbaseDriver()
@@ -21,12 +21,14 @@ object DBManager {
 
   def findUser(username: String): Future[Option[User]] = findById[User]("yester-users", username)
   def findNeedAnalysisObject(naCode: String): Future[Option[NeedAnalysis]] = findById[NeedAnalysis]("yester-need-analyses", naCode)
+  def findCurriculumDevelopmentObject(devCode: String): Future[CurriculumDevelopment] = findById[CurriculumDevelopment]("yester-curricula-dev", devCode)
 
   def findAllProgrammes(): Future[List[Programme]] = findAll[Programme]("yester-programmes", "progr_dd", "prog")
 
   def createProgramme(progKey: String, progData: Programme): Future[OpResult] = save[Programme]("yester-programmes", progKey, progData)
 
   def addOrUpdateNeedAnalysis(key: String, naData: NeedAnalysis): Future[OpResult] = save[NeedAnalysis]("yester-need-analyses", key, naData)
+  def upsertCurriculumDevelopment(key: String, cdData: CurriculumDevelopment): Future[OpResult] = save[CurriculumDevelopment]("yester-curricula-dev", key, cdData)
 
   def findById[T](bucketName: String, docKey: String)(implicit valFormat: Format[T]): Future[Option[T]] = {
       val curBucket = driver.bucket(bucketName)

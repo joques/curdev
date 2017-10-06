9ad4d7d7712a3af2194d2ac2401513f808a94e52
José G. Quenum <josum@icloud.com>
2017-10-06 20:47:07 +0200

    changed curriculum development

--- src/main/scala/lib/CurriculumDevelopment.scala
+++ src/main/scala/lib/CurriculumDevelopment.scala
@@ -2,7 +2,7 @@ package yester.lib
 
 import play.api.libs.json.{Json, Format}
 
-final case class CurriculumDevelopment(pacMembers: Option[List[SingleCommitteeMember]], submissionDate: Option[String], decision: Option[String])
+final case class CurriculumDevelopment(pacMembers: Option[List[SingleCommitteeMember]], cdcMembers: Option[List[SingleCommitteeMember]], submissionDate: Option[String], decision: Option[String])
 
 object CurriculumDevelopmentJsonImplicits {
     implicit val singleComMemberFmt: Format[SingleCommitteeMember] = SingleCommitteeMemberJsonImplicits.singleComFmt

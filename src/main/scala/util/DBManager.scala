54b7009a412b7b059349ae29e334ea31b6c9d8f6
José G. Quenum <josum@icloud.com>
2021-01-22 21:34:52 +0200

    filled more packaged to new db manager

--- src/main/scala/util/DBManager.scala
+++ src/main/scala/util/DBManager.scala
@@ -9,3 +9,15 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import org.reactivecouchbase.ReactiveCouchbaseDriver
+import scala.concurrent.Future
+import org.reactivecouchbase.client.{OpResult, Constants}
+import com.couchbase.client.protocol.views.{Stale, Query}
+import play.api.libs.json.{Json, Format, Writes}
+import net.spy.memcached.{PersistTo, ReplicateTo}
+import yester.lib.{User, UserJsonImplicits, Programme, ProgrammeJsonImplicits, NeedAnalysis, NeedAnalysisJsonImplicits, CurriculumDevelopment, CurriculumDevelopmentJsonImplicits}
+
+object DBManager {
+  
+}

628a9bb4507bcb491717042f4e9c7327dc71beba
José G. Quenum <josum@icloud.com>
2021-01-23 10:18:49 +0200

    merged the packages to be imported

--- src/main/scala/util/DBManager.scala
+++ src/main/scala/util/DBManager.scala
@@ -7,17 +7,21 @@ import play.api.libs.json._
 import akka.stream.scaladsl.Sink
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import com.typesafe.config.ConfigFactory
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import org.reactivecouchbase.ReactiveCouchbaseDriver
 import scala.concurrent.Future
-import org.reactivecouchbase.client.{OpResult, Constants}
-import com.couchbase.client.protocol.views.{Stale, Query}
 import play.api.libs.json.{Json, Format, Writes}
 import net.spy.memcached.{PersistTo, ReplicateTo}
 import yester.lib.{User, UserJsonImplicits, Programme, ProgrammeJsonImplicits, NeedAnalysis, NeedAnalysisJsonImplicits, CurriculumDevelopment, CurriculumDevelopmentJsonImplicits}
 
+
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import org.reactivecouchbase.ReactiveCouchbaseDriver
+
+
+import org.reactivecouchbase.client.{OpResult, Constants}
+import com.couchbase.client.protocol.views.{Stale, Query}
+
+
 object DBManager {
-  
+
 }

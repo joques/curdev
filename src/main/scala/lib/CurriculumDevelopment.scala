13bcac13bd4546adf242b3661a7ebfbc18ddc3c6
José G. Quenum <josum@icloud.com>
2021-01-26 14:34:58 +0200

    removed unnecessary methods

--- src/main/scala/lib/CurriculumDevelopment.scala
+++ src/main/scala/lib/CurriculumDevelopment.scala
@@ -27,31 +27,4 @@ object CurriculumDevelopmentJsonImplicits {
       ),
       JsonWrites[MODELTYPE](jsv => ByteString(Json.stringify(modelFormat.writes(jsv))))
     )
-
-  implicit val defaultPlayJsonConverter: CouchbaseJsonDocConverter[JsValue] = new CouchbaseJsonDocConverter[JsValue] {
-    override def convertTo(ref: AnyRef): JsValue = JsonConverter.convertToJsValue(ref)
-    override def convertFrom(ref: JsValue): Any  = JsonConverter.convertJsonValue(ref)
-  }
-
-  case class PlayJsonQueryParams(query: JsObject = Json.obj()) extends QueryParams {
-    override def isEmpty: Boolean         = query.value.isEmpty
-    override def toJsonObject: JsonObject = JsonConverter.convertToJson(query)
-  }
-
-  implicit class EnhancedJsObject(val obj: JsObject) extends AnyVal {
-    def asQueryParams: PlayJsonQueryParams = PlayJsonQueryParams(obj)
-  }
-
-  implicit class EnhancedJsValue(val value: JsValue) extends AnyVal {
-    def asCbValue: Any = defaultPlayJsonConverter.convertFrom(value)
-  }
-
-  implicit class EnhancedJsonArray(val value: JsonArray) extends AnyVal {
-    def asJsValue: Any = defaultPlayJsonConverter.convertTo(value)
-  }
-
-  implicit class EnhancedJsonObject(val value: JsonObject) extends AnyVal {
-    def asJsValue: Any = defaultPlayJsonConverter.convertTo(value)
-  }
-
 }

18baa53ba07e7255e7bc13cfa6ba96f9067d4994
José G. Quenum <josum@icloud.com>
2021-01-26 14:30:07 +0200

    dumped more code inside the cd implicits

--- src/main/scala/lib/CurriculumDevelopment.scala
+++ src/main/scala/lib/CurriculumDevelopment.scala
@@ -11,8 +11,47 @@ object CurriculumDevelopmentJsonImplicits {
     implicit val cdFmt = Json.format[CurriculumDevelopment]
     implicit val cdWrites = Json.writes[CurriculumDevelopment]
     implicit val cdReads = Json.reads[CurriculumDevelopment]
+		
+	val cdJsonReads: JsonReads[CurriculumDevelopment]   = JsonReads(bs => JsonSuccess(Json.parse(bs.utf8String)))
+  	val cdJsonWrites: JsonWrites[CurriculumDevelopment] = JsonWrites(jsv => ByteString(Json.stringify(jsv)))
+  	implicit val defaultCDFormat: JsonFormat[CurriculumDevelopment] = JsonFormat(cdJsonReads, cdJsonWrites)
 	
-	implicit val cdJsonReads: JsonReads[CurriculumDevelopment] = JsonReads(bs => JsonSuccess(bs))
-	implicit val cdJsonWrites: JsonWrites[CurriculumDevelopment] = JsonWrites(bs => bs)
-	implicit val defaultCDFormat: JsonFormat[CurriculumDevelopment] = JsonFormat(read, write)
+	implicit def convertJsonFormat[MODELTYPE](modelFormat: Format[MODELTYPE]): JsonFormat[MODELTYPE] =
+    JsonFormat[MODELTYPE](
+      JsonReads[MODELTYPE](
+        bs =>
+          modelFormat
+            .reads(Json.parse(bs.utf8String))
+            .map(result => JsonSuccess(result))
+            .getOrElse[JsonResult[MODELTYPE]](JsonError())
+      ),
+      JsonWrites[MODELTYPE](jsv => ByteString(Json.stringify(modelFormat.writes(jsv))))
+    )
+
+  implicit val defaultPlayJsonConverter: CouchbaseJsonDocConverter[JsValue] = new CouchbaseJsonDocConverter[JsValue] {
+    override def convertTo(ref: AnyRef): JsValue = JsonConverter.convertToJsValue(ref)
+    override def convertFrom(ref: JsValue): Any  = JsonConverter.convertJsonValue(ref)
+  }
+
+  case class PlayJsonQueryParams(query: JsObject = Json.obj()) extends QueryParams {
+    override def isEmpty: Boolean         = query.value.isEmpty
+    override def toJsonObject: JsonObject = JsonConverter.convertToJson(query)
+  }
+
+  implicit class EnhancedJsObject(val obj: JsObject) extends AnyVal {
+    def asQueryParams: PlayJsonQueryParams = PlayJsonQueryParams(obj)
+  }
+
+  implicit class EnhancedJsValue(val value: JsValue) extends AnyVal {
+    def asCbValue: Any = defaultPlayJsonConverter.convertFrom(value)
+  }
+
+  implicit class EnhancedJsonArray(val value: JsonArray) extends AnyVal {
+    def asJsValue: Any = defaultPlayJsonConverter.convertTo(value)
+  }
+
+  implicit class EnhancedJsonObject(val value: JsonObject) extends AnyVal {
+    def asJsValue: Any = defaultPlayJsonConverter.convertTo(value)
+  }
+
 }

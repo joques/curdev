d584a8b1c471779d7c2513444cfeabdc7fac2531
José G. Quenum <josum@icloud.com>
2021-01-26 12:43:05 +0200

    trying to fix the json reader issue

--- src/main/scala/lib/CurriculumDevelopment.scala
+++ src/main/scala/lib/CurriculumDevelopment.scala
@@ -1,6 +1,8 @@
 package yester.lib
 
 import play.api.libs.json.{Json, Format}
+import org.reactivecouchbase.rs.scaladsl.json.{JsonReads, JsonWrites, JsonSuccess}
+import foo.bar.jsonlib.{JsonNode, JsonObj}
 
 final case class CurriculumDevelopment(pacMembers: Option[List[SingleCommitteeMember]], cdcMembers: Option[List[SingleCommitteeMember]], submissionDate: Option[String], decision: Option[String])
 
@@ -10,4 +12,8 @@ object CurriculumDevelopmentJsonImplicits {
     implicit val cdFmt = Json.format[CurriculumDevelopment]
     implicit val cdWrites = Json.writes[CurriculumDevelopment]
     implicit val cdReads = Json.reads[CurriculumDevelopment]
+	
+	implicit val cdJsonReads: JsonReads[CurriculumDevelopment] = JsonReads(bs => JsonSuccess(JsonNode.parse(bs.utf8String)))
+	implicit val cdJsonWrites: JsonWrites[CurriculumDevelopment] = JsonWrites(jsv => ByteString(JsonNode.stringify(jsv)))
+	implicit val defaultCDFormat: JsonFormat[CurriculumDevelopment] = JsonFormat(read, write)
 }
--- src/main/scala/lib/NeedAnalysis.scala
+++ src/main/scala/lib/NeedAnalysis.scala
@@ -1,6 +1,8 @@
 package yester.lib
 
 import play.api.libs.json.{Json, Format}
+import org.reactivecouchbase.rs.scaladsl.json.{JsonReads, JsonWrites, JsonSuccess}
+import foo.bar.jsonlib.{JsonNode, JsonObj}
 
 final case class NeedAnalysis(consultations: Option[List[NAConsultationComponent]], survey: Option[NASurveyComponent], conclusion: Option[NAConclusionComponent], bos: Option[NABosComponent], senate: Option[NASenateComponent])
 
@@ -14,4 +16,8 @@ object NeedAnalysisJsonImplicits {
     implicit val naFmt = Json.format[NeedAnalysis]
     implicit val naWrites = Json.writes[NeedAnalysis]
     implicit val naReads = Json.reads[NeedAnalysis]
+	
+	implicit val naJsonReads: JsonReads[NeedAnalysis] = JsonReads(bs => JsonSuccess(JsonNode.parse(bs.utf8String)))
+	implicit val naJsonWrites: JsonWrites[NeedAnalysis] = JsonWrites(jsv => ByteString(JsonNode.stringify(jsv)))
+	implicit val defaultNAFormat: JsonFormat[NeedAnalysis] = JsonFormat(read, write)
 }

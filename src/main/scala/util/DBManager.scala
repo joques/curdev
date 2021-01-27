fd609a976bd8f2893365ec69f55921e29294ad16
José G. Quenum <josum@icloud.com>
2021-01-27 10:10:04 +0200

    forced to initialize a variable

--- src/main/scala/util/DBManager.scala
+++ src/main/scala/util/DBManager.scala
@@ -43,7 +43,7 @@ object DBManager {
 	//rewrite this function to eliminate the inner future
 	def findAllProgrammes(): Future[Seq[Programme]] = {
 		val progSeqFuture: Future[Seq[Future[Programme]]] = findAll[Programme]("yester-programmes", "progr_dd", "prog")
-		var finalRes: Future[Seq[Programme]]; 
+		var finalRes: Future[Seq[Programme]] = null; 
 		progSeqFuture.onComplete {
 			case Failure(progSeqError) => finalRes = Failure(new Exception("Error fetching programme list ", progSeqError))
 			case Success(allProgsFuture) => finalRes = Future.sequence(allProgsFuture)

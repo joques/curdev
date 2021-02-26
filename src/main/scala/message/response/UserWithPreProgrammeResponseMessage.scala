225e615417759cd2776d096847d614ecf60c789b
José G. Quenum <josum@icloud.com>
2021-02-26 17:07:16 +0200

    I do not understand

--- src/main/scala/message/response/UserWithPreProgrammeResponseMessage.scala
+++ src/main/scala/message/response/UserWithPreProgrammeResponseMessage.scala
@@ -2,6 +2,7 @@ package yester.message.response
 
 import com.couchbase.client.scala.implicits.Codec
 // import play.api.libs.json.{Reads, Json, Format}
+
 import yester.lib.UserWithPreProgramme
 
 final case class UserWithPreProgrammeResponseMessage(messageId: String, operationError: Option[String], operationResult: Option[UserWithPreProgramme]) extends ResponseMessage[UserWithPreProgramme](messageId, operationError, operationResult)

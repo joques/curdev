0b01f275cf1878bd64fa7d208381796d3c4b7eb2
José G. Quenum <josum@icloud.com>
2021-02-26 17:19:45 +0200

    uncomment the wrong object

--- src/main/scala/lib/UserWithPreProgramme.scala
+++ src/main/scala/lib/UserWithPreProgramme.scala
@@ -7,20 +7,20 @@ import User._
 
 final case class UserWithPreProgramme(userDetails: User, preProgrammeCodes: Option[Seq[String]])
 
-object UserWithPreProgrammeJsonImplicits {
-    // implicit val userFormat: Format[User] =  User.userFmt
-
-    implicit val uwPPFmt = Json.format[UserWithPreProgramme]
-    implicit val uwPPWrites = Json.writes[UserWithPreProgramme]
-    implicit val uwPPReads = Json.reads[UserWithPreProgramme]
-}
-//
-// object UserWithPreProgramme {
-//     implicit val userWPPCodec: Codec[UserWithPreProgramme] = Codec.codec[UserWithPreProgramme]
-
-//     implicit val userFormat: Format[User] =  UserJsonImplicits.userFmt
+// object UserWithPreProgrammeJsonImplicits {
+//     implicit val userFormat: Format[User] =  User.userFmt
 
 //     implicit val uwPPFmt = Json.format[UserWithPreProgramme]
 //     implicit val uwPPWrites = Json.writes[UserWithPreProgramme]
 //     implicit val uwPPReads = Json.reads[UserWithPreProgramme]
 // }
+//
+object UserWithPreProgramme {
+    implicit val userWPPCodec: Codec[UserWithPreProgramme] = Codec.codec[UserWithPreProgramme]
+
+    implicit val userFormat: Format[User] =  UserJsonImplicits.userFmt
+
+    implicit val uwPPFmt = Json.format[UserWithPreProgramme]
+    implicit val uwPPWrites = Json.writes[UserWithPreProgramme]
+    implicit val uwPPReads = Json.reads[UserWithPreProgramme]
+}

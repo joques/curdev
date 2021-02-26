4645c35460255d585e6d62798d8e18f8e80b3ac8
José G. Quenum <josum@icloud.com>
2021-02-26 15:34:59 +0200

    renamed old DB and fixing more import errors

--- src/main/scala/lib/Summary.scala
+++ src/main/scala/lib/Summary.scala
@@ -3,7 +3,7 @@ package yester.lib
 // import play.api.libs.json.{Json, Format}
 import com.couchbase.client.scala.implicits.Codec
 
-import yester.lib.Programme
+import Programme._
 
 final case class Summary(inProgress: Option[Seq[Programme]], dueForReview: Option[Seq[Programme]], recentlyApproved: Option[Seq[Programme]])
 

bb89d09b4641ed2e54a5410d0a52e437234e5c3f
José G. Quenum <josum@icloud.com>
2021-02-26 16:03:24 +0200

    more and more

--- src/main/scala/message/response/UserResponseMessage.scala
+++ src/main/scala/message/response/UserResponseMessage.scala
@@ -1,14 +1,20 @@
 package yester.message.response
 
-import play.api.libs.json.{Reads, Json, Format}
-import yester.lib.{User, UserJsonImplicits}
+// import play.api.libs.json.{Reads, Json, Format}
+import com.couchbase.client.scala.implicits.Codec
+import yester.lib.User._
 
 final case class UserResponseMessage(messageId: String, operationError: Option[String], operationResult: Option[User]) extends ResponseMessage[User](messageId, operationError, operationResult)
 
-object UserResponseMessageJsonImplicits {
-    implicit val userFormat: Format[User] =  UserJsonImplicits.userFmt
-
-    implicit val userResponseMessageFmt = Json.format[UserResponseMessage]
-    implicit val userResponseMessageWrites = Json.writes[UserResponseMessage]
-    implicit val userResponseMessageReads = Json.reads[UserResponseMessage]
+object UserResponseMessage {
+	implicit val codec: Codec[UserWithPreProgrammeResponseMessage] = Codec.codec[UserWithPreProgrammeResponseMessage]
 }
+
+
+// object UserResponseMessageJsonImplicits {
+//     implicit val userFormat: Format[User] =  UserJsonImplicits.userFmt
+
+//     implicit val userResponseMessageFmt = Json.format[UserResponseMessage]
+//     implicit val userResponseMessageWrites = Json.writes[UserResponseMessage]
+//     implicit val userResponseMessageReads = Json.reads[UserResponseMessage]
+// }
